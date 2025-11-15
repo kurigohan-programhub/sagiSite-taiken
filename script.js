6b3d155016aad97c05615caa2da02a4697cdab32
tamanegi fiver(old kurigohan) <wadoby1@gmail.com>
2025-11-15 19:02:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
 
     console.log("-------------------");
-    console.log("⚠️注意: これはフィッシング詐欺の仕組みの再現です。以下の情報は送信されていませんが、実際の詐欺では悪意のある第三者へ送られます。");
+    console.log("注意: これはフィッシング詐欺の仕組みの再現です。以下の情報は送信されていませんが、実際の詐欺では悪意のある第三者へ送られます。");
     console.log("送信されたメールアドレス: " + email);
     console.log("送信されたパスワード: " + password);
     console.log("-------------------");
